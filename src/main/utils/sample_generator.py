1d47d7c96648bbb474a3c5f54de6ad8aa6f90b2d
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2018-03-01 00:53:27 -0500

    Added Beta Distributiion.

--- src/main/utils/sample_generator.py
+++ src/main/utils/sample_generator.py
@@ -4,7 +4,8 @@ All distributions have a fixed positive support [left, right] such that 0<=left<
 Samples from the following distributions can be generated
  - Normal distribution
  - Beta distribution
- - Exponential
+ - Exponential distribution
+ - Uniform distribution
 
 The module will no start returning the mean as well, since it is hard to control the mean
 
@@ -13,6 +14,8 @@ The module will no start returning the mean as well, since it is hard to control
 import numpy as np
 from scipy.stats import truncnorm
 from scipy.stats import truncexpon
+from scipy.stats import beta
+
 
 class SampleGenerator:
     # TODO: Add code/methods for generating distributions
@@ -40,6 +43,8 @@ class SampleGenerator:
             return self.uniform(N, T)
         elif self.distribution == "exponential":
             return self.exponential(N, T)
+        elif self.distribution == "beta":
+            return self.beta(N, T)
         else:
             raise NotImplementedError
 
@@ -65,17 +70,24 @@ class SampleGenerator:
 
     def exponential(self, N, T):
         """
-        :return: samples from the uniform distribution with support [self.left, self.right]
+        :return: samples from the exponential distribution with support [self.left, self.right]
         Notes - We'll be using exponential with a support 0, 1. To achieve this we'll be
         truncating the exponential distribution after 1. The support of the exponential
         distribution is from 0 to infinity by default. We will be keeping the scale low so that
         not a lot of samples are truncated from the distribution and we have some consistency.
         """
 
-        # Truncexpon moves from 0 to b.
+        # truncexpon moves from 0 to b.
         np.random.seed(self.random_seed)
-        samples = truncexpon.rvs(b=self.right, loc=self.mean, scale=self.stdev, size=(N, T))
-        return samples
+        return truncexpon.rvs(b=self.right, loc=self.mean, scale=self.stdev, size=(N, T))
+
+    def beta(self, N, T):
+        """
+        :return: samples from the beta distribution with with the following parameters
+        a=0.5, b=1, loc=left support, scale=right support (Already tested with these params, they give us a beta with 0,1 support)
+        """
+        np.random.seed(self.random_seed)
+        return beta.rvs(a=0.5, b=1, loc=self.left, scale=self.right, size=(N, T))
 
     def true_mean(self):
         def true_mean_normal():
@@ -88,14 +100,14 @@ class SampleGenerator:
         def true_mean_exponential():
             return truncexpon.mean(b=self.right, loc=self.mean, scale=self.stdev)
 
-        def true_mean_gumbell():
-            print("gumbell")
+        def true_mean_beta():
+            return beta.mean(a=0.5, b=1, loc=self.left, scale=self.right)
 
         compute_true_mean = {
                    "normal": true_mean_normal,
                    "uniform": true_mean_uniform,
                    "exponential": true_mean_exponential,
-                   "gumbell": true_mean_gumbell
+                   "beta": true_mean_beta
                    }
 
         return compute_true_mean[self.distribution]()
